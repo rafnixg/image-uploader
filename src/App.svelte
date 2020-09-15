e45da57095d2dd3e8737d3a2160160fe0a73dee1
Rafnix Guzman <rafnixg@gmail.com>
2020-09-15 00:08:35 -0500

    [ADD] Steps with events

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,27 @@
 <script>
-  import Uploader from "./components/Uploader.svelte"
-  function handleUpload(e){
-    console.log(e.detail)
+  import Uploader from "./components/Uploader.svelte";
+  import ImageUpload from "./components/ImageUpload.svelte";
+  import Loading from "./components/Loading.svelte";
+
+  let state = "initial";
+  console.log(state);
+  function handleUpload(e) {
+    if (e.detail === "uploading") {
+      state = "loading";
+    } else if (e.detail === "upload") {
+      state = "upload";
+    } else {
+      state = "initial";
+    }
+
+    console.log(e.detail);
   }
 </script>
-<Uploader on:upload={handleUpload}/>
+
+{#if state === 'initial'}
+  <Uploader on:upload={handleUpload} />
+{:else if state === 'loading'}
+  <Loading />
+{:else if state === 'upload'}
+  <ImageUpload />
+{/if}
